486f27caf7ff83136880b83229d483d3e632cfde
salgadu <rodrigosdc20.ma@hotmail.com>
2022-05-10 23:08:58 -0300

    Tarefa 06, 08

--- Tarefa Aula 08/modo_retrato.py
+++ Tarefa Aula 08/modo_retrato.py
@@ -6,8 +6,16 @@ borrado = cv2.GaussianBlur(image, (31, 31), 0)
 
 mask = np.zeros(image.shape, np.uint8)
 mask = cv2.circle(mask, (910, 250), 250, (255, 255,255), -1)
+mask = cv2.GaussianBlur(mask, (31, 31), 0)
+mask_inv = cv2.bitwise_not(mask) 
 
-output = np.where(mask == np.array([255, 255, 255]), image, borrado)
+fundo = borrado * (mask_inv / 255)
+fundo = fundo.astype(np.uint8)
+
+foco = image * (mask / 255)
+foco = foco.astype(np.uint8)
+
+output = (foco + fundo)
 
 cv2.imshow('Imagem Noise', image)
 cv2.imshow('Imagem Boa M', output)
